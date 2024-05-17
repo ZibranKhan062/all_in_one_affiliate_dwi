8e2acdb76eec906a95578f96fc836756271fb38f
zibrankhan <zibrangni@gmail.com>
2024-05-17 16:40:34 +0530

    updated with Refer and Earn complete

--- app/src/main/java/com/allshopping/app/ProfileActivity.java
+++ app/src/main/java/com/allshopping/app/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.allshopping.app;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -46,11 +48,26 @@ public class ProfileActivity extends AppCompatActivity {
     private StorageReference mStorage;
     private ProgressBar progressBar;
     private Uri selectedImageUri;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Find the MaterialToolbar by its ID
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Enable the back button in the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Initialize Firebase components
         mAuth = FirebaseAuth.getInstance();
